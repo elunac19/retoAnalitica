bb37746b6c9186a17b8fc44f88580cdbc12a4f47
elunac19 <elunac19@gmail.com>
2023-09-14 09:00:53 -0600

    Actividad de Obtencion Descriptivas

---
+++ Obtención de estadísticasDescriptivas.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pandas as pd
+
+df = pd.read_csv("openpowerlifting.csv")
+
+#Aqui tuve que poner abs value, debido a que algunos values dan negativos pero es por error de sistema de obtencion de datos. 
+df['BestSquatKg'] = df['BestSquatKg'].abs()
+df['BestBenchKg'] = df['BestBenchKg'].abs()
+df['BestDeadliftKg'] = df['BestDeadliftKg'].abs()
+
+columns_describe = ['Sex','Equipment','Age','BodyweightKg','WeightClassKg','BestSquatKg','BestBenchKg','BestDeadliftKg']
+
+#Aquí se puede ver una breve representación de los datos obtenidos.
+print( df.head() )
+
+#Aquí se pueden apreciar más claramente el tipo de variables que se encuentran en los datos.
+print( df.info() )
+
+# Este comando es para imprimir la información general sobre los datos, como el promedio, max, min, 
+# la diferencia de valores entre las variables (std) y el número de valores contados.
+print( df[columns_describe].describe() )
